931895eedb57b1717249216da1cfb68c2f4075e8
Karik <karik.grant@gmail.com>
2025-03-04 08:32:50 -0500

    More small changes to part 4

--- Lab 7/Lab 7 - Part4.py
+++ Lab 7/Lab 7 - Part4.py
@@ -9,33 +9,45 @@ grnLed = machine.Pin(14, machine.Pin.OUT)
 
 buzzer = machine.Pin(13, machine.Pin.OUT)
 
+alarming = False
+
 def pir_handler(pin):
     #The handler gets the triggered pin that we can compare
     #against our defined pins above
     if pin == pir1:
         print("Alarm! Motion detected in Zone 1!!")
+        alarming = True
         for i in range (10):
             redLed.toggle()
             utime.sleep_ms(20)
             for j in range (20):
                 buzzer.toggle()
-                utime.sleep_ms(2)
+                #changed this time to make the buzzer on/off more apparent
+                utime.sleep_ms(20)
+        alarming = False
     elif pin == pir2:
         print("Alarm! Motion detected in Zone 2!!")
-            for i in range (10):
-                grnLed.toggle()
+        alarming = True
+        for i in range (10):
+            grnLed.toggle()
+            utime.sleep_ms(20)
+            for j in range (20):
+                buzzer.toggle()
+                #changed this time to make the buzzer on/off more apparent
                 utime.sleep_ms(20)
-                for j in range (20):
-                    buzzer.toggle()
-                    utime.sleep_ms(2)
+        alarming = False
     else:
         print("This should never happen :)")
+    print("Alarm ended!")
 
 pir1.irq(trigger=machine.Pin.IRQ_RISING, handler=pir_handler)
 pir2.irq(trigger=machine.Pin.IRQ_RISING, handler=pir_handler)
 
 grnLed.on()
-while True:
-    redLed.toggle()
-    grnLed.toggle()
-    utime.sleep(1)
+
+while not alarming:
+    print("Alarm active, no motion detected.")
+    for i in range(10):
+        redLed.toggle()
+        grnLed.toggle()
+        utime.sleep(1)
